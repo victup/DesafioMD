1112de2734f81e8dae6f8ccf358ca79bab16f1a6
victup <victor.gsantos@hotmail.com>
2022-12-23 00:53:42 -0300

    ajuste botão limpar

--- Triangulo/Triangulo.Presentation/frmTela.Designer.cs
+++ Triangulo/Triangulo.Presentation/frmTela.Designer.cs
@@ -96,6 +96,7 @@
             this.btnLimpar.TabIndex = 5;
             this.btnLimpar.Text = "Limpar";
             this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Visible = false;
             this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
             // lblLado1
